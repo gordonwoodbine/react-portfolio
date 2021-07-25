768662ec0c9f66047096fbd7a0386b1dc2b75b0a
Mike Lightfoot <gordon_woodbine@yahoo.co.uk>
2021-07-25 18:23:54 +0100

    change about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -4,24 +4,24 @@ import style from './About.module.scss';
 const containerVariants = {
   hidden: {
     x: '100vw',
-    opacity: 0
+    opacity: 0,
   },
   visible: {
     x: 0,
     opacity: 1,
-    transition: { 
-      type: 'spring', 
+    transition: {
+      type: 'spring',
       mass: 0.4,
       damping: 8,
       when: 'beforeChildren',
-      staggerChildren: 0.4
-    }
+      staggerChildren: 0.4,
+    },
   },
   exit: {
     x: '-100vw',
-    transition: { ease: 'easeInOut' }
-  }
-}
+    transition: { ease: 'easeInOut' },
+  },
+};
 
 // const childVariants = {
 //   hidden: {
@@ -44,14 +44,27 @@ const About = () => {
       exit='exit'
     >
       <div className={style.left}>
-        <div className="box">
+        <div className='box'>
           <h2>A Little Bit About Me</h2>
         </div>
-        <p>As you may have worked out by this point, my name is Mike. I decided to become a web developer last year - I was working in retail when the world fell apart and I realised it was time for a change. I've always loved technology, I like figuring out how things work and I enjoy solving problems so I figured web development could be a good fit for me.</p>
-        <p>Since then I've been teaching myself as much as I can. The web is a wonderful place with no shortage of kind people willing to share their skills and knoweldge.</p>
+        <p>
+          As you may have worked out by this point, my name is Mike. I decided
+          to become a web developer last year - I was working in retail when the
+          virus arrived and, as I'd been wanting to make a change for a while,
+          it was just the kick up the arse I needed. So since then I've been
+          busy relearning <span className={style.highlight}>HTML</span> and
+          <span className={style.highlight}> CSS</span> (because I first learned
+          them circa 2001 and, you know, things have changed a bit), learning
+          JavaScript
+        </p>
+        <p>
+          Since then I've been teaching myself as much as I can. The web is a
+          wonderful place with no shortage of kind people willing to share their
+          skills and knoweldge.
+        </p>
       </div>
     </motion.section>
-  )
+  );
 };
 
 export default About;
